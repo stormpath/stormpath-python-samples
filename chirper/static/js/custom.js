b5ceac672e86f541577fe8b145bdf921a35c26b1
Goran Cetusic <goran.cetusic@dobarkod.hr>
2013-08-23 17:49:04 +0200

    Error message on invalid token reset

--- chirper/static/js/custom.js
+++ chirper/static/js/custom.js
@@ -11,20 +11,23 @@ $(document).ready(function () {
 });
 
 function updateChirps() {
-    $(".chirps").empty();
-    $.ajax({
-        url: chirps_url,
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            if (response.length !== 0) {
-                response.forEach(function (chirp) {
-                    $(".chirps").append(chirp.message);
-                });
-            } else {
-                html = "<div class='alert'>There are no chirps!</div>";
-                $(".chirps").append(html);
+    $(".chirps").fadeOut(function() {
+        $(this).empty();
+        $.ajax({
+            url: chirps_url,
+            type: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                if (response.length !== 0) {
+                    response.forEach(function (chirp) {
+                        $(".chirps").append(chirp.message);
+                    });
+                } else {
+                    html = "<div class='alert'>There are no chirps!</div>";
+                    $(".chirps").append(html);
+                }
             }
-        }
+        });
     });
+    $(".chirps").fadeIn();
 }
